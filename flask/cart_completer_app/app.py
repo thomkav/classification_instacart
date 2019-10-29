589ec38b17a42ed4b15ad9204d654cee8e0da3f8
Thomas Kavanagh <thomaskavanagh.dev@gmail.com>
2019-10-28 21:23:58 -0400

    save progress after first MVP

--- flask/cart_completer_app/app.py
+++ flask/cart_completer_app/app.py
@@ -16,10 +16,11 @@ assert api.convert_temp_from_unit
 app = Flask(__name__)  # create instance of Flask class
 CORS(app)
 
+# with open('cart_completer_app/test.txt', 'w') as file:
+#     file.write("test")
 
-
-with open('./data/sample_user_orders.pkl', 'rb') as file:
-    sample_user_orders = pickle.load(file)
+with open('cart_completer_app/data/merged_user_orders.pkl', 'rb') as file:
+    merged_orders = pickle.load(file)
 
 @app.route('/')  # the site to route to
 def api_test():
@@ -29,44 +30,144 @@ def api_test():
 
     return render_template('user-viewer.html')    
 
-@app.route('/get_user_order', methods=['GET','POST'])  # the site to route to
+
+#########################
+# RETURN ACTUAL USER CART
+#########################
+@app.route('/get_user_order_actual', methods=['GET','POST'])
 def get_user_orders():
-    with open('data/sample_user_orders.pkl','rb') as file:
-        sample_user_orders = pickle.load(file)
+    # with open('data/actual_user_orders.pkl','rb') as file:
+    #     sample_user_orders = pickle.load(file)
     
     user_id = int(request.args.get('user_id'))
-    print(type(user_id))
-    print(sample_user_orders)
-    print("-----")
-    print('user_id = ', user_id)
-    print(sample_user_orders[sample_user_orders.user_id == user_id])
-    return sample_user_orders[sample_user_orders.user_id == user_id].to_html()
 
+    # Remove rows where add_to_cart_order is NaN.
+    _ = merged_orders.dropna()
 
-@app.route('/view-user')  # the site to route to
-def present_user_orders():
+    table_output = (
+        _[_.user_id == user_id][['product_name', 'add_to_cart_order','prev_order_ct']]
+        ).sort_values('add_to_cart_order')
 
-    tables = [sample_user_orders.to_html(classes='ts1')]
-    titles = ['sample user orders']
+    html_output = table_output.style.set_table_styles(
+            [
+                {'selector': 'thead th',
+            'props': [('background-color', 'AliceBlue')]},
+                {'selector': 'thead th',
+            'props': [('background-color', 'AliceBlue')]},
+                ]).hide_index().render()
 
-    return render_template('user-viewer.html',tables=tables, titles=titles)        
+    status = 200
 
-@app.route('/convert_temperature', methods=['POST'])
-def convert_temps():
-    "Only route defined for this Flask app!"
-    print(f"Input is {request.json}")
+    return html_output, status
 
+##############################
+# FILTER / RETURN PREDICTION #
+##############################
+@app.route('/get_cart_prediction', methods=['GET','POST'])
+def get_cart_prediction():
     try:
-        result = api.convert_temp_from_unit(request.json['temperature'], request.json['unit'])
-        status = 200
-    except KeyError:
-        result = {
-            'message': 'Need to have input keys temperature and unit. Unit must be K, F, or C',
-            'input': request.json
-        }
-        status = 400
+        # with open('data/model_preds.pkl','rb') as file:
+        #     sample_user_orders = pickle.load(file)
+        user_id = int(request.args.get('user_id'))
+        
+        _ = (merged_orders[merged_orders.user_id == user_id]
+        ).sort_values('user_id').drop(
+            labels=['product_id','user_id','order_id_ref','order_id','prev_order_ct'],
+            axis=1
+            )
 
-    return jsonify(result), status
+        try:
+            thresh = float(request.args.get('thresh'))
+            print('got threshold values:',thresh)
+        except:
+            thresh = 0.5
+        
+        # Filter Threshold
+        _ = _[_.pred_prob > thresh]
+        _.sort_values(by=['pred_prob'], ascending=False, inplace=True)
+
+        html_output = _.style.set_table_styles(
+            [
+                {'selector': 'thead th',
+            'props': [('background-color', 'AliceBlue')]},
+                {'selector': 'thead th',
+            'props': [('background-color', 'AliceBlue')]},
+                ]).hide_index().render()
+
+        status = 200
+        return html_output, status
+    except:
+        status = 400
+        return None, status
+
+
+# #########################
+# # RETURN PREDICTION
+# #########################
+# @app.route('/get_cart_prediction', methods=['GET','POST'])
+# def get_cart_prediction():
+#     try:
+#         with open('data/model_preds.pkl','rb') as file:
+#             sample_user_orders = pickle.load(file)
+#         user_id = int(request.args.get('user_id'))
+        
+#         df = (
+#             sample_user_orders
+#             [sample_user_orders.user_id == user_id]
+#             ).sort_values('user_id')[['product_name', 'order_prob']]
+
+#         html_output = df.style.set_table_styles(
+#             [
+#                 {'selector': 'thead th',
+#             'props': [('background-color', 'AliceBlue')]},
+#                 {'selector': 'thead th',
+#             'props': [('background-color', 'AliceBlue')]},
+#                 ]).hide_index().render()
+
+#         status = 200
+#         return html_output, status
+#     except:
+#         status = 400
+#         return None, status
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/view-user')  # the site to route to
+# def present_user_orders():
+
+#     tables = [sample_user_orders.to_html(classes='ts1')]
+#     titles = ['sample user orders']
+
+#     return render_template('user-viewer.html',tables=tables, titles=titles)        
+
+
+
+# @app.route('/convert_temperature', methods=['POST'])
+# def convert_temps():
+#     "Only route defined for this Flask app!"
+#     print(f"Input is {request.json}")
+
+#     try:
+#         result = api.convert_temp_from_unit(request.json['temperature'], request.json['unit'])
+#         status = 200
+#     except KeyError:
+#         result = {
+#             'message': 'Need to have input keys temperature and unit. Unit must be K, F, or C',
+#             'input': request.json
+#         }
+#         status = 400
+
+#     return jsonify(result), status
 
 
 
--- flask/cart_completer_app/static/js/functions.js
+++ flask/cart_completer_app/static/js/functions.js
@@ -1,67 +1,115 @@
-function getUserOrders(){
-    // These names have to match the keys expected by Python
-    // This is the json object that is passed to the `/temperature`
-    // route.
-    console.log('test');
+// User Slider - Update Tables
+function userSlider() {
 
-    const user_order_request = {
-        'user_id': parseInt($('#user_id input').val()),
-    };
+    getUserCartActual();
+    getUserCartPrediction();
+    
+}
 
-    // const features = {
-    //   'temperature': parseFloat($('#temp').val()),
-    //   'unit': $('#unit').val()
-    // };
-  
-    /*
-     * url: where we send the request
-     * data: the JSON/"dictionary" object that Flask sees in request.json
-     * contentType: mandatory. Let's us know that we are passing JSON data
-     * success: the function that is called if the call is successful (i.e. response code is 2XX)
-     * error: the function that is called if the call is unsuccessful (i.e. response code is not 2XX)
-     *
-     * Note: whatever flask returns from the "convert_temps()" function is passed back as an
-     * argument to the success/error functions.
-     */
+// Get Actual User Cart
+function getUserCartActual() {
+
+    const user_id = parseInt($('#user_id').val());
+
+    console.log(user_id);
+
+    $("#viewing_user").html(user_id);
 
     $.ajax({
-        url: '/get_user_order',
+        url: '/get_user_order_actual',
         type: 'GET',
-        data: {'user_id': user_order_request.user_id},
+        data: { 'user_id': user_id },
         dataType: 'text',
-        success: (result) => user_order_get_success(result),
-        error: (result) => user_order_get_fail(result)
+        success: (result) => getUserCartActual_Success(result),
+        error: (result) => getUserCartActual_Fail(result)
     });
 
-  }
+}
 
-function user_order_get_success(result){
+// ADD USER ORDER TABLE TO DIV
+function getUserCartActual_Success(result) {
     console.log('Success!');
-    $("#user_order").html(result)
-  }
-  
-  // Function called on error
-  // You can decide what you'd like
-function user_order_get_fail(result){
+    $("#user_order_actual").html(result)
+}
+
+function getUserCartActual_Fail(result) {
     console.log('There was an error! Our application returned');
-    console.log(result.toString().slice(-400,0));
-  }
+    console.log(result.toString().slice(-400, 0));
+}
+
+function getUserCartPrediction() {
+
+    const user_order_request = {
+        'user_id': parseInt($('#user_id').val()),
+        'thresh': parseInt($("#thresh-slider").val()) / 100.0
+    };
+
+    $.ajax({
+        url: '/get_cart_prediction',
+        type: 'GET',
+        data: { 'user_id': user_order_request.user_id,
+        'thresh': user_order_request.thresh},
+        dataType: 'text',
+        success: (html_output) => getUserCartPrediction_Success(html_output),
+        error: (html_output) => getUserCartPrediction_Fail(html_output)
+    });
+}
+
+// ADD USER PREDICTIONS TO DIV
+function getUserCartPrediction_Success(html_output) {
+    console.log('Success!');
+    $("#user_order_prediction").html(html_output);
+}
+
+function getUserCartPrediction_Fail(table) {
+    console.log('There was an error! Our application returned');
+    console.log(table.toString().slice(-400, 0));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
  * This is called whenever the number or the unit are changed
  */
-function doConversion(){
+function doConversion() {
     // These names have to match the keys expected by Python
     // This is the json object that is passed to the `/temperature`
     // route.
     console.log('test')
 
     const features = {
-      'temperature': parseFloat($('#temp').val()),
-      'unit': $('#unit').val()
+        'temperature': parseFloat($('#temp').val()),
+        'unit': $('#unit').val()
     };
-  
+
     /*
      * url: where we send the request
      * data: the JSON/"dictionary" object that Flask sees in request.json
@@ -82,19 +130,19 @@ function doConversion(){
         error: (result) => metis_error(result)
     });
 
-  }
-  
-  // Function called on success.
-  // Your job: instead of printing result to the console, make it print to
-  //           the div with id "result".
-  function metis_success(result){
-    const msg = `This temperature is ${result['F']} in Farenheight, ${result['C']} in Centigrade and ${result['K']} in Kelvin`; 
+}
+
+// Function called on success.
+// Your job: instead of printing result to the console, make it print to
+//           the div with id "result".
+function metis_success(result) {
+    const msg = `This temperature is ${result['F']} in Farenheight, ${result['C']} in Centigrade and ${result['K']} in Kelvin`;
     $('#result').html(msg);
-  }
-  
-  // Function called on error
-  // You can decide what you'd like
-  function metis_error(result){
+}
+
+// Function called on error
+// You can decide what you'd like
+function metis_error(result) {
     console.log('There was an error! Our application returned');
     console.log(result.responseJSON);
-  }
+}
